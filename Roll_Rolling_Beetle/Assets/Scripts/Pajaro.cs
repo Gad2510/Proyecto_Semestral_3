fda80153cea308e9cf1e8d19a691f4bfffab1c3b
AzaelSan <AzaelSan@users.noreply.github.com>
2020-03-22 19:24:01 -0600

    Pajaro
    Agregue al enemigo Pajaro, su prefab y una escena de prueba.
    Cambia de posicion cada 6 segundos.
    Mata al jugador si entra en su rango (trigger) y se mantiene por 3 segundos.

--- Roll_Rolling_Beetle/Assets/Scripts/Pajaro.cs
+++ Roll_Rolling_Beetle/Assets/Scripts/Pajaro.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class Pajaro : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float contador = 0.0f; //Contador para cambiar de posicion si no entra el jugador en si trigger
+    public float contadorAtaque = 0.0f; //Contador para atacar al jugador si esta dentro se su trigger
+    bool atacando = false; //Saber si esta atacando
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!atacando)
+        {
+            contador += Time.deltaTime;//Sumar
+
+            if (contador >= 6.0f)
+            {
+                CambiarPos();
+                contador = 0.0f;
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            atacando = true; //Atacar
+
+            contadorAtaque += Time.deltaTime;//Sumar
+
+            if (contadorAtaque >= 3.0f)
+            {
+                contadorAtaque = 0.0f;
+                Destroy(other.gameObject); //Condicion de derrota
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            atacando = false;
+            contadorAtaque = 0.0f;
+        }
+    }
+
+    public void CambiarPos()
+    {
+        int pos;
+        pos = Random.Range(-50,50);
+
+        gameObject.transform.position = new Vector3(pos, gameObject.transform.position.y, gameObject.transform.position.z);
     }
 }
