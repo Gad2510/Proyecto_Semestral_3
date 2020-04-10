e402506109df4bd7a73de99248876f959598a5c1
Gabriel <gad2510@hotmail.com>
2020-04-09 22:03:36 -0500

    Implementacion de sombras en el juego
    Se creo un shader que setea las sombras en todo el mapa a traves de una mascara que renderiza mascaras en todo el mapa

---
+++ Roll_Rolling_Beetle/Assets/Scripts/Cordanates.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cordanates : MonoBehaviour
+{
+    Material groundMaterial;
+    public Vector4 cordenadas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        groundMaterial = GetComponent<MeshRenderer>().material;
+        groundMaterial.SetVector("_Cord", cordenadas);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Roll_Rolling_Beetle/Assets/Scripts/CrearMapa.cs
+++ Roll_Rolling_Beetle/Assets/Scripts/CrearMapa.cs
@@ -8,31 +8,70 @@ public class CrearMapa : MonoBehaviour
     
     void Start()
     {
-        GameObject g1 = Instantiate(terrenoD.ObtenerTerreno(), null); //Crear terreno 1
-        g1.transform.localPosition = new Vector3(0, 0, 0); //Posicion de terreno 1
+        GameObject g;
+        Vector3 coord=Vector4.zero;
 
-        GameObject g2 = Instantiate(terrenoD.ObtenerTerreno(), null); //Crear terreno 2
-        g2.transform.localPosition = new Vector3(0, 0, 100); //Posicion de terreno 2
+        for(int x = 0; x < 3; x++)
+        {
+            coord.x = x;
+            for(int y = 0; y < 3; y++)
+            {
+                coord.z = y;
 
-        GameObject g3 = Instantiate(terrenoD.ObtenerTerreno(), null); //Crear terreno 3
-        g3.transform.localPosition = new Vector3(0, 0, 200); //Posicion de terreno 3
+                if(x==1 && y == 1)
+                {
+                    g = Instantiate(terrenoD.terrenos[17], null); //Crear terreno 5
+                }
+                else
+                {
+                    g = Instantiate(terrenoD.ObtenerTerreno(), null);
+                    
+                }
+                g.transform.localPosition = coord * 100f;
+                SetCoord(coord, g.transform);
+            }
+        }
+        /*g = Instantiate(terrenoD.ObtenerTerreno(), null); //Crear terreno 1
+        g.transform.localPosition = new Vector3(0, 0, 0); //Posicion de terreno 1
+        
+        g = Instantiate(terrenoD.ObtenerTerreno(), null); //Crear terreno 2
+        g.transform.localPosition = new Vector3(0, 0, 100); //Posicion de terreno 2
 
-        GameObject g4 = Instantiate(terrenoD.ObtenerTerreno(), null); //Crear terreno 4
-        g4.transform.localPosition = new Vector3(100, 0, 0); //Posicion de terreno 4
+        g = Instantiate(terrenoD.ObtenerTerreno(), null); //Crear terreno 3
+        g.transform.localPosition = new Vector3(0, 0, 200); //Posicion de terreno 3
 
-        GameObject g5 = Instantiate(terrenoD.terrenos[17], null); //Crear terreno 5
-        g5.transform.localPosition = new Vector3(100, 0, 100); //Posicion de terreno 5
+        g = Instantiate(terrenoD.ObtenerTerreno(), null); //Crear terreno 4
+        g.transform.localPosition = new Vector3(100, 0, 0); //Posicion de terreno 4
 
-        GameObject g6 = Instantiate(terrenoD.ObtenerTerreno(), null); //Crear terreno 6
-        g6.transform.localPosition = new Vector3(100, 0, 200); //Posicion de terreno 6
+        g = Instantiate(terrenoD.terrenos[17], null); //Crear terreno 5
+        g.transform.localPosition = new Vector3(100, 0, 100); //Posicion de terreno 5
 
-        GameObject g7 = Instantiate(terrenoD.ObtenerTerreno(), null); //Crear terreno 7
-        g7.transform.localPosition = new Vector3(200, 0, 0); //Posicion de terreno 7
+        g = Instantiate(terrenoD.ObtenerTerreno(), null); //Crear terreno 6
+        g.transform.localPosition = new Vector3(100, 0, 200); //Posicion de terreno 6
 
-        GameObject g8 = Instantiate(terrenoD.ObtenerTerreno(), null); //Crear terreno 8
-        g8.transform.localPosition = new Vector3(200, 0, 100); //Posicion de terreno 8
+        g = Instantiate(terrenoD.ObtenerTerreno(), null); //Crear terreno 7
+        g.transform.localPosition = new Vector3(200, 0, 0); //Posicion de terreno 7
 
-        GameObject g9 = Instantiate(terrenoD.ObtenerTerreno(), null); //Crear terreno 9
-        g9.transform.localPosition = new Vector3(200, 0, 200); //Posicion de terreno 9
+        g = Instantiate(terrenoD.ObtenerTerreno(), null); //Crear terreno 8
+        g.transform.localPosition = new Vector3(200, 0, 100); //Posicion de terreno 8
+
+        g = Instantiate(terrenoD.ObtenerTerreno(), null); //Crear terreno 9
+        g.transform.localPosition = new Vector3(200, 0, 200); //Posicion de terreno 9*/
+    }
+
+    private void SetCoord(Vector3 coord, Transform g)
+    {
+        Vector4 loc = new Vector4(2-coord.x, 2-coord.z, 3f, 0f);
+        Material r;
+        bool isGround = false;
+        for(int i=0;i<g.childCount && !isGround; i++)
+        {
+            Transform child = g.GetChild(i);
+            isGround = child.CompareTag("Ground");
+            if(isGround){
+                r = child.GetComponent<MeshRenderer>().material;
+                r.SetVector("_Cord", loc);
+            }
+        }
     }
 }
