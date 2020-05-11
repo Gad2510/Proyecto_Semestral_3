bf6af6df2a33a0258bbcae289da4c45d7a07ab13
brianfs12 <47504868+brianfs12@users.noreply.github.com>
2020-05-11 00:40:01 -0500

    implementacion de audioManager(falta Gamemanager)

--- Roll_Rolling_Beetle/Assets/Scripts/AudioManager.cs
+++ Roll_Rolling_Beetle/Assets/Scripts/AudioManager.cs
@@ -1,18 +1,79 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+
+    [Range(0f,1f)]
+    public float volume = 0.7f;
+    [Range(0.5f,1.5f)]
+    public float pitch = 1f;
+
+    [Range(0f, 0.5f)]
+    public float randomVolume = 0.1f;
+    [Range(0f,.5f)]
+    public float randomPitch = 0.1f;
+
+    private AudioSource source;
+
+    public void SetSource(AudioSource _source)
+    {
+        source = _source;
+        source.clip = clip;
+    }
+
+    public void Play()
+    {
+        source.volume = volume * (1 + Random.Range(-randomVolume / 2f, randomVolume / 2f));
+        source.pitch = pitch * (1 + Random.Range(-randomPitch / 2f, randomPitch / 2f));
+        source.Play();
+    }
+
+
+}
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static AudioManager instance;
+    [SerializeField]
+    Sound[] sounds;
+
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogError("more than one AudioManager is in the scene");
+        }
+        else
+        {
+            instance = this;
+        }
+    }
     void Start()
     {
-        
+
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            GameObject gameObject = new GameObject("Sound_" + i + "_" + sounds[i].name);
+            gameObject.transform.parent.SetParent(this.transform);
+            sounds[i].SetSource(gameObject.AddComponent<AudioSource>());
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlaySound(string _name)
     {
-        
+        for(int i = 0; i< sounds.Length; i++)
+        {
+            if(sounds[i].name == _name)
+            {
+                sounds[i].Play();
+                return;
+            }
+        }
+        Debug.LogWarning("audiosManager: Sonido no encontrado en la lista:" + _name);
     }
+
 }
