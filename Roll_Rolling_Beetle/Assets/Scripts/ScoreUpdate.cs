cc63af3afc1dad6cf9cab7a0d9184688a07164f2
AzaelSan <AzaelSan@users.noreply.github.com>
2020-05-22 01:34:51 -0500

    Pantalla de Game Over

--- Roll_Rolling_Beetle/Assets/Scripts/ScoreUpdate.cs
+++ Roll_Rolling_Beetle/Assets/Scripts/ScoreUpdate.cs
@@ -2,21 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro; 
 
 public class ScoreUpdate : MonoBehaviour
 {
-    public Text score, highscore;
+    public TextMeshProUGUI score, highscore;
 
-    // Start is called before the first frame update
     void Start()
     {
         score.text = "Puntuacion: \n" + Scene_Manager_BH._instance.settings.score;
         highscore.text = "Highscore: \n" + Scene_Manager_BH._instance.settings.highscore;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
