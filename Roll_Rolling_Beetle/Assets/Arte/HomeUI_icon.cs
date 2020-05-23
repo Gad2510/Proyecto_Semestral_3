aa4239ed10e3c58de40a0e7881ea3ae79f1c0b32
AzaelSan <AzaelSan@users.noreply.github.com>
2020-05-23 00:01:11 -0500

    Icono de casa

--- Roll_Rolling_Beetle/Assets/Arte/HomeUI_icon.cs
+++ Roll_Rolling_Beetle/Assets/Arte/HomeUI_icon.cs
@@ -19,7 +19,7 @@ public class HomeUI_icon : MonoBehaviour
         if(CacaPorcentage.value >= 0.9f && !escalando)
         {
             escalando = true;
-            LeanTween.scale(gameObject, new Vector3(0.5f, 0.5f, 0.5f), 1.0f).setLoopPingPong();
+            LeanTween.scale(gameObject, new Vector3(0.5f, 0.5f, 0.5f), 0.3f).setLoopPingPong();
         }
 
         //Si no se esta escalando, dejar su escala en 1
