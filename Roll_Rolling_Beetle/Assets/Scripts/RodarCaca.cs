88fd146fe1a30c4fd660d4f843b1691f4f4b8b35
AzaelSan <AzaelSan@users.noreply.github.com>
2020-06-15 22:59:12 -0500

    Coffe

--- Roll_Rolling_Beetle/Assets/Scripts/RodarCaca.cs
+++ Roll_Rolling_Beetle/Assets/Scripts/RodarCaca.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RodarCaca : MonoBehaviour //Hace girar la caca, el script esta dentro del modelo y usa de referencia el objeto de centro como pivote de rotacion solo sirve tras ser lanzada la caca
+public class RodarCaca : MonoBehaviour
 {
+    //Hace girar la caca, el script esta dentro del modelo y usa de referencia el objeto de centro como pivote de rotacion solo sirve tras ser lanzada la caca
     Personaje escarabajo;
     float CacaRotVel;
     public GameObject centro;
